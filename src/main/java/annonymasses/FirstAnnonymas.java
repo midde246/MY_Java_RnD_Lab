8317a2e4b76c9b1ad477d8696ecca4a24da0aeb0
Sarada Prasad Midde <spmidde@gmail.com>
2020-03-27 00:18:40 +0530

    First Commit.

---
+++ src/main/java/annonymasses/FirstAnnonymas.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package annonymasses;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author saradam
+ */
+public class FirstAnnonymas {
+    public static void main(String[] args) {
+//        Map<String> map = new HashMap<>{put("0")};
+    }
+}
